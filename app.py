689d7d149db633981f173192de87fdeb4916f462
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-19 13:34:15 -0300

    garantindo janela maximizada e funcao de atalho

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from time import sleep
 
 def clicar(x: int, y:int):
     pg.click(x=x, y=y, duration=2)
-    sleep(1)
+    sleep(2)
 
 
 def pressionar_botao(botao:str):
     if botao in pg.KEYBOARD_KEYS:
         pg.press(botao)
-        sleep(1)
+        sleep(2)
     else: 
         raise Exception('Botão inválido. Esse botão não existe na biblioteca')
 
@@ -21,11 +21,20 @@ def pressionar_botao(botao:str):
 def digitar_texto(texto: str): 
     pyperclip.copy(texto)
     pg.hotkey('ctrl', 'v')
-    sleep(1)
+    sleep(2)
 
 
 def esperar_pagina_carregar():
     sleep(40)
+
+
+def executar_atalho(botao1, botao2):
+    if (botao1 in pg.KEYBOARD_KEYS) and (botao2 in pg.KEYBOARD_KEYS):
+        pg.hotkey(botao1, botao2)
+        sleep(2)
+    else: 
+        raise Exception('Um ou mais botões inválidos. Digite um botão válido.')
+
 
 
 # Abrindo o navegador:
@@ -33,6 +42,9 @@ pressionar_botao('win')
 digitar_texto('Google Chrome')
 pressionar_botao('enter')
 clicar(x=302, y=643)
+executar_atalho('alt', 'space')
+[pressionar_botao('up') for i in range(7)]
+pressionar_botao('enter')
 
 # Acessando o site: https://file-examples.com/
 digitar_texto('https://file-examples.com/')
